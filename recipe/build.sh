b1469813f0604609aa1455f0cc2d4fdd80d2464d
Paul Yim <paul.j.yim@gmail.com>
2021-02-16 19:29:38 +0000

    Add bootstrapping template; move deps under bootstrapping template; only build libcurl when bootstrapping.

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,21 +8,23 @@ fi
 # need macosx-version-min flags set in cflags and not cppflags
 export CFLAGS="$CFLAGS $CPPFLAGS"
 
-#./configure \
-#    --prefix=${PREFIX} \
-#    --host=${HOST} \
-#    --disable-ldap \
-#    --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
-#    --with-ssl=${PREFIX} \
-#    --with-ssl \
-#    --with-zlib=${PREFIX} \
-#    --with-gssapi=${PREFIX} \
-#    --with-libssh2=${PREFIX} \
-#    --with-nghttp2=${PREFIX} \
-#|| cat config.log
-
-./configure \
-    --prefix=${PREFIX}
+if [[ $BOOTSTRAPPING == yes ]]; then
+  ./configure \
+      --prefix=${PREFIX}
+else
+  ./configure \
+      --prefix=${PREFIX} \
+      --host=${HOST} \
+      --disable-ldap \
+      --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
+      --with-ssl=${PREFIX} \
+      --with-ssl \
+      --with-zlib=${PREFIX} \
+      --with-gssapi=${PREFIX} \
+      --with-libssh2=${PREFIX} \
+      --with-nghttp2=${PREFIX} \
+  || cat config.log
+fi
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 # TODO :: test 1119... exit FAILED

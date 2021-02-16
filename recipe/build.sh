323a555a55e313c92d3b1f2fbf293b0f4814d49b
Paul Yim <paul.j.yim@gmail.com>
2021-02-16 17:55:30 +0000

    save

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  # Get an updated config.sub and config.guess
+  cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
+fi
 
 # need macosx-version-min flags set in cflags and not cppflags
 export CFLAGS="$CFLAGS $CPPFLAGS"
 
+#./configure \
+#    --prefix=${PREFIX} \
+#    --host=${HOST} \
+#    --disable-ldap \
+#    --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
+#    --with-ssl=${PREFIX} \
+#    --with-ssl \
+#    --with-zlib=${PREFIX} \
+#    --with-gssapi=${PREFIX} \
+#    --with-libssh2=${PREFIX} \
+#    --with-nghttp2=${PREFIX} \
+#|| cat config.log
+
 ./configure \
-    --prefix=${PREFIX} \
-    --host=${HOST} \
-    --disable-ldap \
-    --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
-    --with-ssl=${PREFIX} \
-    --with-zlib=${PREFIX} \
-    --with-gssapi=${PREFIX} \
-    --with-libssh2=${PREFIX} \
-    --with-nghttp2=${PREFIX} \
-|| cat config.log
+    --prefix=${PREFIX}
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 # TODO :: test 1119... exit FAILED
